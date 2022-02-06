dd0dab9eb1d07e0b9955e2f935ece9954f4620b2
Carlos Felipe Borges Mesquita <cafeborges@outlook.com>
2022-02-06 18:16:50 -0300

    corrigindo um erro

--- src/main/java/br/com/askowiks/classificacaocampeonatofutebol/ResultadoPartida.java
+++ src/main/java/br/com/askowiks/classificacaocampeonatofutebol/ResultadoPartida.java
@@ -3,7 +3,7 @@ package br.com.askowiks.classificacaocampeonatofutebol;
 public enum ResultadoPartida {
     VITORIA(3), EMPATE(1), DERROTA(0);
 
-    private int valor;
+    private final int valor;
 
     ResultadoPartida(int valor){
         this.valor = valor;
